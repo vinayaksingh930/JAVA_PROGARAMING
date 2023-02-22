750cfc99d17f1c035863d6001badc38e31a71f37
vinayaksingh930 <113895193+vinayaksingh930@users.noreply.github.com>
2023-02-22 23:56:14 +0530

    Create PATTERN_NUMBER_2.java

---
+++ PATTERN_NUMBER_2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+//pattern number:
+public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter");
+        int a=sc.nextInt();
+
+        for(int i=1 ;i<=a;i++){ 
+            for(int j=1 ;j<=i;j++){
+                
+        
+            System.out.print(j+" ");
+        }
+     System.out.println();
+    }
+}
+}
